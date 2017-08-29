ac097044f3ee2bdd797de2caa3432b1e9e87c620
Belette69 <tristan.leclercq@hotmail.fr>
2017-08-29 13:29:51 +0200

    css commit

--- templates/subTemplates/eleve/eleveForm.php
+++ templates/subTemplates/eleve/eleveForm.php
@@ -8,18 +8,20 @@ if(!isset($eleve) || !($eleve instanceof Eleve))
 ?>
 <form action="" method="post" class="eleve-form">
     <p>
-        <label for="nom">Nom</label><input <?php echo isset($invalidFields) && in_array('nom', $invalidFields) ? 'class="error"' : ""?> type="text" required="required" name="nom" id="nom" value="<?php echo htmlentities($eleve->getNom(), ENT_QUOTES);?>"/>
+        
+        <label for="nom">Nom</label><input <?php echo isset($invalidFields) && in_array('nom', $invalidFields) ? 'class="error"' : ""?> type="text" required="required" class="form-control" name="nom" id="nom" value="<?php echo htmlentities($eleve->getNom(), ENT_QUOTES);?>"/>
+        
     </p>
     <p>
-        <label for="prenom">Prénom</label><input <?php echo isset($invalidFields) && in_array('prenom', $invalidFields) ? 'class="error"' : ""?>  type="text" required="required" name="prenom" id="prenom" value="<?php echo htmlentities($eleve->getPrenom(), ENT_QUOTES);?>"/>
+        <label for="prenom">Prénom</label><input <?php echo isset($invalidFields) && in_array('prenom', $invalidFields) ? 'class="error"' : ""?>  type="text" required="required" class="form-control" name="prenom" id="prenom" value="<?php echo htmlentities($eleve->getPrenom(), ENT_QUOTES);?>"/>
     </p>
     <p>
-        <label for="email">Email</label><input <?php echo isset($invalidFields) && in_array('email', $invalidFields) ? 'class="error"' : ""?>  type="email" required="required" name="email" id="email" value="<?php echo htmlentities($eleve->getEmail(), ENT_QUOTES);?>"/>
+        <label for="email">Email</label><input <?php echo isset($invalidFields) && in_array('email', $invalidFields) ? 'class="error"' : ""?>  type="email" required="required" class="form-control" placeholder="exemple@exemple.com" name="email" id="email" value="<?php echo htmlentities($eleve->getEmail(), ENT_QUOTES);?>"/>
     </p>
 
 
         </select>
     </p>
-    <p class="submit-container"><input type="submit" value="OK"/></p>
+    <p class="submit-container"><input type="submit" value="VALIDER"/></p>
     <input type="hidden" name="id" value="<?php echo htmlentities($eleve->getId()); ?>"/>
 </form>
--- templates/subTemplates/secretariat/secretariatForm.php
+++ templates/subTemplates/secretariat/secretariatForm.php
@@ -8,27 +8,27 @@ if(!isset($secretariat) || !($secretariat instanceof Secretariat))
 ?>
 <form action="" method="post" class="user-form">
     <p>
-        <label for="nom">Nom</label><input <?php echo isset($invalidFields) && in_array('nom', $invalidFields) ? 'class="error"' : ""?> type="text" required="required" name="nom" id="nom" value="<?php echo htmlentities($secretariat->getNom(), ENT_QUOTES);?>"/>
+        <label for="nom">Nom </label><input <?php echo isset($invalidFields) && in_array('nom', $invalidFields) ? 'class="error"' : ""?> type="text" required="required" class="form-control" name="nom" id="nom" value="<?php echo htmlentities($secretariat->getNom(), ENT_QUOTES);?>"/>
     </p>
     <p>
-        <label for="prenom">Prénom</label><input <?php echo isset($invalidFields) && in_array('prenom', $invalidFields) ? 'class="error"' : ""?>  type="text" required="required" name="prenom" id="prenom" value="<?php echo htmlentities($secretariat->getPrenom(), ENT_QUOTES);?>"/>
+        <label for="prenom">Prénom </label><input <?php echo isset($invalidFields) && in_array('prenom', $invalidFields) ? 'class="error"' : ""?>  type="text" required="required" name="prenom" class="form-control" id="prenom" value="<?php echo htmlentities($secretariat->getPrenom(), ENT_QUOTES);?>"/>
     </p>
     <p>
-        <label for="email">Email</label><input <?php echo isset($invalidFields) && in_array('email', $invalidFields) ? 'class="error"' : ""?>  type="email" required="required" name="email" id="email" value="<?php echo htmlentities($secretariat->getEmail(), ENT_QUOTES);?>"/>
+        <label for="email">Email </label><input <?php echo isset($invalidFields) && in_array('email', $invalidFields) ? 'class="error"' : ""?>  type="email" required="required" name="email" class="form-control" id="email" value="<?php echo htmlentities($secretariat->getEmail(), ENT_QUOTES);?>" placeholder="exemple@exemple.com"/>
     </p>
     <p>
-        <label for="numero">Numero</label><input <?php echo isset($invalidFields) && in_array('numero', $invalidFields) ? 'class="error"' : ""?>  type="numero" required="required" name="numero" id="numero" value="<?php echo htmlentities($secretariat->getNumero(), ENT_QUOTES);?>"/>
+        <label for="numero">Numero </label><input <?php echo isset($invalidFields) && in_array('numero', $invalidFields) ? 'class="error"' : ""?>  type="numero" required="required" name="numero" class="form-control" id="numero" value="<?php echo htmlentities($secretariat->getNumero(), ENT_QUOTES);?>"/>
     </p>
     <p>
-        <label for="pseudo">Pseudo</label><input <?php echo isset($invalidFields) && in_array('pseudo', $invalidFields) ? 'class="error"' : ""?>  type="pseudo" required="required" name="pseudo" id="pseudo" value="<?php echo htmlentities($secretariat->getPseudo(), ENT_QUOTES);?>"/>
+        <label for="pseudo">Pseudo </label><input <?php echo isset($invalidFields) && in_array('pseudo', $invalidFields) ? 'class="error"' : ""?>  type="pseudo" required="required" name="pseudo" class="form-control" id="pseudo" value="<?php echo htmlentities($secretariat->getPseudo(), ENT_QUOTES);?>"/>
     </p>
     <p>
-        <label for="password">Password</label><input <?php echo isset($invalidFields) && in_array('password', $invalidFields) ? 'class="error"' : ""?>  type="password" required="required" name="password" id="password" value="<?php echo htmlentities($secretariat->getPassword(), ENT_QUOTES);?>"/>
+        <label for="password">Password </label><input <?php echo isset($invalidFields) && in_array('password', $invalidFields) ? 'class="error"' : ""?>  type="password" required="required" name="password" id="password" value="<?php echo htmlentities($secretariat->getPassword(), ENT_QUOTES);?>" type="password" class="form-control" placeholder="Password"/>
     </p>
 
 
         </select>
     </p>
-    <p class="submit-container"><input type="submit" value="OK"/></p>
+    <p class="submit-container"><input type="submit" value="VALIDER"/></p>
     <input type="hidden" name="id" value="<?php echo htmlentities($secretariat->getId()); ?>"/>
 </form>
